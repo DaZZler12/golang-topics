813c7e04e690469fc378b690b14064d0e3c52f25
Dazzler12 <abhirupbhattacharya8@gmail.com>
2024-08-07 02:53:24 +0530

    wait group

--- concurrency/wait_groups/main.go
+++ concurrency/wait_groups/main.go
@@ -1,2 +1,31 @@
+// waitGroup
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func Process(wg *sync.WaitGroup, itr int) {
+	fmt.Println("Hello from process routine, going to sleep & it number: ", itr)
+	time.Sleep(1 * time.Second)
+	fmt.Println("Weak up, going to end & itr number: ", itr)
+	wg.Done()
+}
+
+func main() {
+
+	// wait Groups are used to block the termination of the main go routine
+	// do it actually block the main go-routine untill all the child routines are completed
+	wg := &sync.WaitGroup{}
+	counter := 3
+	for counter > 0 {
+		wg.Add(1)
+		go Process(wg, counter)
+		counter--
+	}
+	wg.Wait() // this will be blocked until the counter becomes 0, i.e. All the go-routines ends
+	fmt.Println("terminating main routine")
+
+}
